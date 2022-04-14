3c47460a7b4f5cee1b6ef9c5604b22f381925fa5
Suhaib B <suhaib.bog84@yahoo.com>
2022-04-14 15:24:08 -0400

    day2 - Tes dash boars numbers

---
+++ src/test/java/com/cydeo/day2/LibraryDashBoardTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.day2;
+
+import org.testng.annotations.Test;
+
+public class LibraryDashBoardTest {
+
+    @Test
+    public void testDashboardNumbers(){
+
+        // Dashboard numbers are previously not matching the database numbers
+        // the test is meant to check
+        // the actual results from dashboard page matches database results
+
+
+
+    }
+}
